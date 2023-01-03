b2453fcc15382f0b337a4963f71e9b96b21ed8f6
cgalang <cgalang11@hotmail.com>
2023-01-03 15:14:18 -0800

    Add loading screen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, View, StyleSheet, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { initializeApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import Landing from "./components/auth/Landing";
 import Register from "./components/auth/Register";
 import LogIn from "./components/auth/Login";
-import { Entypo } from "@expo/vector-icons";
+// import { Entypo } from "@expo/vector-icons";
+import { useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 import {
   API_KEY,
@@ -33,51 +35,90 @@ if (getApps().length < 1) {
   const app = initializeApp(firebaseConfig);
 }
 
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100,
+  },
+});
+
 const Stack = createNativeStackNavigator();
 export default function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(user);
+      setIsLoaded(true);
+      setIsLoggedIn(true);
+    } else {
+      setIsLoaded(true);
+      setIsLoggedIn(false);
+    }
+  });
+
+  if (!isLoaded) {
+    return (
+      <View style={styles.wrapper}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (!isLoggedIn) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRoute="Landing">
+          <Stack.Screen
+            name="Landing"
+            component={Landing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={Register}
+            // options={({ navigation }) => ({
+            //   headerLeft: null,
+            //   headerRight: () => (
+            //     <TouchableOpacity onPress={() => navigation.pop()}>
+            //       <Entypo
+            //         name="cross"
+            //         color="blue"
+            //         size={30}
+            //         style={{ paddingHorizontal: 10 }}
+            //       />
+            //     </TouchableOpacity>
+            //   ),
+            // })}
+          />
+          <Stack.Screen
+            name="Log In"
+            component={LogIn}
+            // options={({ navigation }) => ({
+            //   headerLeft: null,
+            //   headerLeft: () => (
+            //     <TouchableOpacity onPress={() => navigation.pop()}>
+            //       <Entypo
+            //         name="cross"
+            //         color="blue"
+            //         size={30}
+            //         style={{ paddingHorizontal: 10 }}
+            //       />
+            //     </TouchableOpacity>
+            //   ),
+            // })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRoute="Landing">
-        <Stack.Screen
-          name="Landing"
-          component={Landing}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Register"
-          component={Register}
-          // options={({ navigation }) => ({
-          //   headerLeft: null,
-          //   headerRight: () => (
-          //     <TouchableOpacity onPress={() => navigation.pop()}>
-          //       <Entypo
-          //         name="cross"
-          //         color="blue"
-          //         size={30}
-          //         style={{ paddingHorizontal: 10 }}
-          //       />
-          //     </TouchableOpacity>
-          //   ),
-          // })}
-        />
-        <Stack.Screen
-          name="Log In"
-          component={LogIn}
-          // options={({ navigation }) => ({
-          //   headerLeft: null,
-          //   headerLeft: () => (
-          //     <TouchableOpacity onPress={() => navigation.pop()}>
-          //       <Entypo
-          //         name="cross"
-          //         color="blue"
-          //         size={30}
-          //         style={{ paddingHorizontal: 10 }}
-          //       />
-          //     </TouchableOpacity>
-          //   ),
-          // })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.wrapper}>
+      <Text>User Logged In</Text>
+    </View>
   );
 }
