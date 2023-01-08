6467154ce21f248870bb0cb312d68d46b77e3285
cgalang <cgalang11@hotmail.com>
2023-01-08 09:11:54 -0800

    Add camera access to app

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,10 +1,100 @@
-import React from "react";
-import { View, Text } from "react-native";
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
 
 export default function Add() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+  const [camera, setCamera] = useState(null);
+  const [imageUri, setImageUri] = useState(null);
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      console.log(data.uri);
+      setImageUri(data.uri);
+    }
+  };
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
+
   return (
-    <View>
-      <Text>Add</Text>
+    <View style={styles.container}>
+      <Camera
+        style={styles.camera}
+        type={type}
+        ratio={"1:1"}
+        ref={(ref) => setCamera(ref)}
+      >
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.takePicBtn} onPress={takePicture}>
+            <Text style={styles.text}>Take Pic</Text>
+          </TouchableOpacity>
+        </View>
+        {imageUri && <Image source={{ uri: imageUri }} style={{ flex: 1 }} />}
+      </Camera>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  takePicBtn: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
